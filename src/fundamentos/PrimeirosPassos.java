a9dedcf9373dfa3d880e61844c7ed2d3838ac769
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2024-10-24 18:21:45 +0200

    Primeiro Commit

---
+++ src/fundamentos/PrimeirosPassos.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+/**
+ * Essa classe representa o JAVA DOC
+ *
+ * @author Rodrigo P. Cardoso <email.com> </email.com>
+ */
+
+  public class PrimeirosPassos{
+
+    public static void main(String[] args) {
+
+        //conheço 3 modos de add comentarios // -> /* e /**
+        //Uma sentença de codigo sempre termina com ;
+        //Mais um comentario
+        //Fim
+
+        /*
+         * Linha 1
+         * Linha 2
+         * Linha 3
+         */
+        System.out.println("Primeiros Passos #1");
+        System.out.println("Primeiros Passos #2");
+    }
+}
