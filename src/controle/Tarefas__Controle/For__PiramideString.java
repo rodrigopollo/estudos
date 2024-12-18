3846e03a63c152c90b385f9a0d7542a4b4964414
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2024-12-18 19:28:33 +0100

    Primeiro Commit

---
+++ src/controle/Tarefas__Controle/For__PiramideString.java
@@ -0,0 +1,12 @@
+package controle.Tarefas__Controle;
+
+public class For__PiramideString {
+    public static void main(String[] args) {
+        // fazer uma pirade com for, sem usar numeros na formula.
+
+        for (String a = "#"; !a.equals("#####"); a += "#") {
+            System.out.println(a);
+
+        }
+    }
+}
