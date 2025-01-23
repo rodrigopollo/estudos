1e0997ad881213c431773cf08a6c99412c34bcd5
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-01-23 14:37:48 +0100

    Repassando tudo após férias.

--- src/fundamentos/Tipo__String.java
+++ src/fundamentos/Tipo__String.java
@@ -1,8 +1,12 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class Tipo__String {
     public static void main(String[] args) {
-
+        
+        
+        System.out.println("Ola pessoal".charAt(1));
         /*
          .charAt nesse caso mostra a letra relacionada
          zero = letra O
@@ -10,28 +14,28 @@ public class Tipo__String {
          2 = letra A
          3 = o espaço em branco entre ola pessoal etc....
          */
-        System.out.println("Ola pessoal".charAt(1));
         /*
            String eh um OBJETO IMUTAVEL, ou seja o valor ORIGINAL que vc coloca nela nao pode ser alterado.
            Ele pode ser substituido ja seja editando a linha string manualmente ou adicionando outra linha
            para substituir o valor original de string, mas vc esta SUBSTITUIND e nao ALTERANDO o valor
            que voce deu para STRING. EX:
          */
+        
         String s = "Boa tarde"; //valor original = Boa tarde
-       //  s = "Bom dia";
+        //  s = "Bom dia";
         //  valor original SUBSTITUIDO por Bom dia. O Valor original contia sendo Boa tarde.
-
+        
         System.out.println(s.concat("!!!")); //Voce esta add "!!!" no final da string "Boa tarde" da linha 21
-        System.out.println(s + "???"); // mesma coisa de maneira diferente, add !!! no final da string
+        System.out.println(s + "???"); // mesma coisa de maneira diferente, add ??? no final da string "Boa tarde"
         System.out.println(s.startsWith("Boa"));//Esta verificando se a string começa com "Boa", -- TRUE --
         System.out.println(s.startsWith("boa"));//eh "Boa" com letra B mauiscula entao, -- FALSE --
         System.out.println(s.toLowerCase().startsWith("boa"));//transforma a string en letras minusculas entao -- True --
-        System.out.println(s.length());//Comando valido so em STRING, verifica qntidade de caracteres
+        System.out.println(s.length());//Comando valido so em STRING, verifica qnt de caracteres, conta espaço.
         System.out.println(s.toUpperCase().endsWith("DIA"));//transforma em maiuscula e verifica se a
-        // string termina com "dia" -- FALSE-- eh "tarde"
+        // string termina com "DIA" -- FALSE-- eh "TARDE"
         System.out.println(s.equals("boa tarde"));//verifica se a string eh igual, -- FALSE -- eh "Boa tarde"
         System.out.println(s.equalsIgnoreCase("boa tarde"));//verifica ignorando CASE entao -- TRUE --
-
+        
         // .startWith() --> retorna um valor de TRUE or FALSE verificando o inicio da string
         // .endsWith() --> retorna um valor de TRUE or FALSE verificando o final da string
         // .toLowerCase() --> transforma a string em letras minusculas
@@ -41,11 +45,11 @@ public class Tipo__String {
         // .length() --> verifica quantos caracteres tem a string selecionada
         // .equals() --> verifica se a string eh igual ao digitado dentro de EQUALS
         // .equalsIgnoreCase() --> ignora maisculas e minusculas e verifica se a string é igual (TRUE or FALSE)
-
+        
         var nome = "Pedro";
-        var sobrenome = "Santos";
+        var sobrenome = "dos Santos";
         var idade = 33;
-        var salario = 12345.987;
+        var salario = 12_345.987;
         //Dessa maneira vc so imprimi as variaveis
         //
         System.out.printf("O senhor %s %s tem %d anos e recebe R$%.2f", nome, sobrenome, idade, salario);
@@ -55,9 +59,12 @@ public class Tipo__String {
         System.out.println(frase);
         //Aqui vc atribui td pra variavel -- maisUmaFrase -- e usa o modelo de info, pula linha, info etc...
         // o \n pula pra proxima linha.
-        String maisUmaFrase = "Nome: "+ nome +"\nSobrenome: "+ sobrenome +"\nIdade: "+ idade +"\nSalario: "+ salario;
+        String maisUmaFrase = "Nome: " + nome
+                + "\nSobrenome: " + sobrenome
+                + "\nIdade: " + idade
+                + "\nSalario: " + salario;
         System.out.println(maisUmaFrase);
-
-
+        
+        
     }
 }
