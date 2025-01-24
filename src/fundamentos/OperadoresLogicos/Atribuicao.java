9999a11f4fd4a9e6769edbc2329c75e0864db39f
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-01-24 13:59:36 +0100

    Repassando tudo após férias.

--- src/fundamentos/OperadoresLogicos/Atribuicao.java
+++ src/fundamentos/OperadoresLogicos/Atribuicao.java
@@ -4,13 +4,13 @@ public class Atribuicao {
     public static void main(String[] args) {
 
         int a = 3; // o valor literal 3 esta atribuido a "VARIAVEL A" do tipo Primitivo INT
-        int b = a; // a "VARIAVEL B" recebe o valor da "VARIAVEL A" do tipo Primitivo INT
-        int c = a + b; // a "variavel C" recebe o valor da expressao "a + b"
+        int b = a; // a "VARIAVEL B" recebe o valor da "VARIAVEL A (3)" do tipo Primitivo INT
+        int c = a + b; // a "variavel C" recebe o valor da expressao "a + b" (3 + 3 = 6)
 
-        c += b; // c = c + b;
-        c -= a; // c = c - b;
-        c *= b; // c = c * b;
-        c /= a; // c = c / b;
+        c += b; // c = c + b;  c(6) + b(3) = 9
+        c -= a; // c = c - a;  c(9) 1 a(3) = 6
+        c *= b; // c = c * b;  c(6) * b(3) = 18
+        c /= a; // c = c / b;  c(6) / a(3) = 6
         c %= 2; // c = c % 2; se a divisao der 0 eh par, se nao eh impar.
 
         System.out.println(c);
