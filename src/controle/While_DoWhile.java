f34d62e1a16eb3a3ad2df9f801a1657f0a40b1ab
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-02-01 20:58:21 +0100

    Repassando tudo após férias.

--- src/controle/While_DoWhile.java
+++ src/controle/While_DoWhile.java
@@ -12,29 +12,30 @@ public class While_DoWhile {
         Diferente do while... o (do while) ele obrigatoriamente executa pelo menos 1x
         Vejamos em detalhes o do while:
 
-        - do {  -->  Execute os statemensts dentro dobloco {...} No caso da (linha 37) ate (linha 40)
-        - Linha 38  -->  O primeiro statement a ser executado
-        - Linha 39  -->  O segundo statement a ser executado
+        - do {  -->  Execute os statemensts dentro do bloco { } do
+        - Linha 38  -->  texto != sair, o programa pergunta quer sair?
+        - Linha 39  -->  se voce digitar qualquer coisa diferente de SAIR (String texto) ele ira continuar,
+        se for SAIR ele finaliza o while (linha 41)
         - }  -->  fecha e termina o bloco (do)
-        - while  --> Apos terminar de executar o DO, o programa diz, comprove se a seguinte
-        expressao eh TRUE or FALSE.
-
-        while (!texto.equalsIgnoreCase("sair"));
-        --> Se a (String texto) for DIFERENTE de (sair) entao continue.[]
+        
+        - while (Linha 41) --> Apos terminar de executar o DO, o programa diz, se a String texto
+        eh igual a SAIR... TRUE (sim) or FALSE(nao).
+              ==>  while (!texto.equalsIgnoreCase("sair"));  <==  texto = sair? TRUE OR FALSE?
+              
+        --> Se a (String texto) for DIFERENTE de sair (FALSE) entao continue.[]
 
         texto = entrada.nextLine(); -> texto eh oq voce ira digitar, se for qualquer coisa
-        que nao seja a palvra (sair), ele ira retornar para a linha 38 e perguntar novamente
+        que nao seja a palavra (sair), ele ira retornar para a linha 38 e perguntar novamente
 
         Deseja continuar?, se digitar sair ele comprova que TEXTO = sair entao pula
-        para a linha 43 e agrade por participar terminando o programa.
-
+        para a linha 43  agrade por participar e termina o programa.
          */
         Scanner entrada = new Scanner(System.in);
 
         String texto;
 
         do {
-            System.out.println("Deseja continuar?");
+            System.out.println("Deseja continuar ou sair?");
             texto = entrada.nextLine();
         }
         while (!texto.equalsIgnoreCase("sair"));
