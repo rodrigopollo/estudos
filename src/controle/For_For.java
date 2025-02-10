3c6d5612124a59b702545573a296f53c13df6191
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-02-10 23:00:57 +0100

    Repassando tudo após férias.

--- src/controle/For_For.java
+++ src/controle/For_For.java
@@ -2,36 +2,37 @@ package controle;
 
 public class For_For {
     public static void main(String[] args) {
-        /* Dentro do laço FOR ---- (laço) ----- do for esta sendo usado a variavel i do tipo INT,
-         vc nao pode usar denovo a variavel i dentro do mesmo scopo.
-         Mas o for te posibilita de usar a variavel i novamente sempre que seja fora do scopo como no
-         exemplo abaixo.
-
+        /* Dentro do laço FOR ---- (laço) ----- esta sendo usado a variavel i do tipo INT,
+         vc NAO pode usar denovo a variavel i dentro do mesmo scopo, mas sim dentro de outro
+         FOR diferente ja que a variavel esta presa dentro do FOR e fora do resto do programa.
          */
+        
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+        // VAR i usada novamente, como esta fora do FOR onde foi usada antes n tem problema.
         int i = 1;
         System.out.println("Saiu do for....");
         System.out.println(i);
 
-
-        //Outra maneira de usar o for eh colocar a variavel fora do fora, mas nao eh comum
+        //Outra maneira de usar o for eh colocar a variavel fora do FOR, mas nao eh comum
+        // ja que antes de fazer isso eh melhor usar FOR-IT (veremos mais adiante)
         int i1 = 1;
         for (; i1 <= 10; i1++) {
             System.out.println(i1);
         }
-
-        /* A terceiro jeito eh FOR dentro de FOR.
-        Executa o 1º) FOR => i2 = 1
-        Executa o 2º) FOR => J = 1 ate cumprir a regra desse for, ou seja ate j ser <= 10. Quando cumprir.
-        Executa o 1º) FOR novamente => i2 = 1 se torna i2 = 2
-        Executa o 2º) FOR novamente => e repete o mesmo processo da linha 27.
-        Isso ira acontecer ate que i2 = 10 e j = 10, entao ele terminara o programa.
+        /* O terceiro jeito eh FOR dentro de FOR.
+        Executa o 1º) FOR => A = 1
+        Executa o 2º) FOR => B = 1 e seguira ate cumprir a regra desse for, ate q B seja = 11.
+        EXEMPLO: A = 1
+                 B = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10...Quando vai chegar a 11, ele volta e recomeça.
+        Executa o 1º) FOR novamente => A = 1 se torna A = 2
+        Executa o 2º) FOR novamente => e repete o mesmo processo da linha 28.
+        Isso ira acontecer ate que A = 10 e B = 10, entao ele terminara o programa.
          */
-        for (int i2 = 1; i2 <= 10; i2++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.printf("[%d ¨%d]", i2, j);
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1; b <= 10; b++) {
+                System.out.printf("[%d ¨%d]", a, b);
             }
         }
 
